98c95d485ffad6c4e51bd98cc7ca59829c110a1a
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2018-08-22 17:37:24 -0300

    Revert "code smell"
    This reverts commit 2639ddb7d30b83bb1eab6abd44bc942487f02d37.

--- Oragon.AspNetCore.Hosting.AMQP/ContextAdapter.cs
+++ Oragon.AspNetCore.Hosting.AMQP/ContextAdapter.cs
@@ -49,7 +49,7 @@ namespace Oragon.AspNetCore.Hosting.AMQP
 
             foreach (var header in props.Headers)
             {
-                if (header.Key.StartsWith("AMQP_") == false)
+                if (!header.Key.StartsWith("AMQP_"))
                 {
                     requestBuilder.AddHeader(header.Key, header.Value.UTF8GetString());
                 }
